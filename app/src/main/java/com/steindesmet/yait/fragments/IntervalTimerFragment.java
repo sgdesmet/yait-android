f9bbd67a0ff04062b6bb51fc29cd13b95d4cfc97
Stein Desmet <stein.desmet@lin-k.net>
2016-10-19 16:11:24 +0200

    placeholder fragment, try to fix intellij build issues with java7/8

---
+++ app/src/main/java/com/steindesmet/yait/fragments/IntervalTimerFragment.java
@@ -0,0 +1,13 @@
+package com.steindesmet.yait.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Date: 19/10/2016
+ * Time: 16:01
+ *
+ * @author sgdesmet1
+ */
+public class IntervalTimerFragment extends Fragment {
+
+}
