ed250517c726fe968cf0469640d61d349f2da342
Stein Desmet <stein.desmet@lin-k.net>
2016-10-19 16:48:02 +0200

    fix import

--- app/src/main/java/com/steindesmet/yait/activities/IntervalTimerActivity.java
+++ app/src/main/java/com/steindesmet/yait/activities/IntervalTimerActivity.java
@@ -1,7 +1,8 @@
 package com.steindesmet.yait.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.steindesmet.yait.R;
 
 public class IntervalTimerActivity extends AppCompatActivity {
 
