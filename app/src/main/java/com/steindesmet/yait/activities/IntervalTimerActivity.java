f85ca28c47255d55b17db44e9b365c6b839842a9
Stein Desmet <stein.desmet@lin-k.net>
2016-10-19 16:45:52 +0200

    more placeholder files

---
+++ app/src/main/java/com/steindesmet/yait/activities/IntervalTimerActivity.java
@@ -0,0 +1,13 @@
+package com.steindesmet.yait.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntervalTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interval_timer);
+    }
+}
